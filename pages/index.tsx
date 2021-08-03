770cc097d0dcdec415dc29786f091dbd5c89cf18
nbr41to <nbr.41to@gmail.com>
2021-08-03 23:38:01 +0900

    .

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,14 @@
+import Image from 'next/image';
+
 export default function Home() {
   return (
     <div>
       <h1>Progress Rec</h1>
+      <Image
+        src={'/dan-cristian-padure-Oj7nnln5k3k-unsplash.jpg'}
+        width={200}
+        height={200}
+      />
     </div>
   );
 }
