be6e10bc943bcf443d82b8236846820ac104ebf1
nbr41to <nbr.41to@gmail.com>
2021-08-06 18:43:30 +0900

    .

--- pages/index.tsx
+++ pages/index.tsx
@@ -38,15 +38,16 @@ const Home = () => {
   }
 
   const googleLogin = () => {
-    Auth.federatedSignIn({ provider: 'Google' });
+    // Auth.federatedSignIn({ provider: 'Google' });
   };
 
   return (
     <div>
-      <Image
+      <img
         src={'/dan-cristian-padure-Oj7nnln5k3k-unsplash.jpg'}
         width={200}
         height={200}
+        alt=''
       />
       <button onClick={googleLogin}>Google Login</button>
       <button onClick={() => Auth.federatedSignIn()}>Login</button>
@@ -68,4 +69,4 @@ const Home = () => {
 };
 
 export default Home;
-export default withAuthenticator(Home);
+// export default withAuthenticator(Home);
